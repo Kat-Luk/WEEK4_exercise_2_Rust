3ddb97f1c0c948400bc9262282cad52a6b12d965
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-25 09:33:49 +0200

    Improve output formatting for file read operation

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
             return;
         } else {
             match read_file(&args[1]) {
-                Ok(contents) => println!("The contents of the file:\n{}\n", contents),
+                Ok(contents) => println!("The contents of the file:\n\n{}", contents),
                 Err(e) => println!("Error reading file: {}", e),
             }
         }
